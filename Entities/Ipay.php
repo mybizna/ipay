0611cf648419a10f4d82540962a5e635f68c90e1
Dedan <dedanirungu@gmail.com>
2024-08-05 11:55:47 +0300

    Changes to files: Entities/Ipay.php database/migrations/2024_08_03_081817_create_ipay_ipay_table.php

--- Entities/Ipay.php
+++ Entities/Ipay.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Ipay\Entities;
 
-use Illuminate\Database\Schema\Blueprint;
 use Modules\Base\Entities\BaseModel;
 
 class Ipay extends BaseModel
@@ -20,42 +19,5 @@ class Ipay extends BaseModel
      * @var string
      */
     protected $table = "Ipay";
-
-    /**
-     * List of fields to be migrated to the datebase when creating or updating model during migration.
-     *
-     * @param Blueprint $table
-     * @return void
-     */
-    public function fields(Blueprint $table = null): void
-    {
-        $this->fields = $table ?? new Blueprint($this->table);
-
-        $this->fields->increments('id')->html('hidden');
-        $this->fields->string('name')->html('text');
-        $this->fields->string('description')->html('textarea');
-        $this->fields->string('published')->html('switch');
-        $this->fields->string('item_id')->nullable()->html('text');
-        $this->fields->string('status')->nullable()->html('text');
-        $this->fields->string('txncd')->nullable()->html('text');
-        $this->fields->string('ivm')->nullable()->html('text');
-        $this->fields->string('qwh')->nullable()->html('text');
-        $this->fields->string('afd')->nullable()->html('text');
-        $this->fields->string('poi')->nullable()->html('text');
-        $this->fields->string('uyt')->nullable()->html('text');
-        $this->fields->string('ifd')->nullable()->html('text');
-        $this->fields->string('agd')->nullable()->html('text');
-        $this->fields->string('mc')->nullable()->html('text');
-        $this->fields->string('p1')->nullable()->html('text');
-        $this->fields->string('p2')->nullable()->html('text');
-        $this->fields->string('p3')->nullable()->html('text');
-        $this->fields->string('p4')->nullable()->html('text');
-        $this->fields->foreignId('payment_id')->nullable()->html('recordpicker')->relation(['payment']);
-        $this->fields->boolean('is_processed')->nullable()->html('switch');
-    }
-
- 
-
-
 
 }
--- database/migrations/2024_08_03_081817_create_ipay_ipay_table.php
+++ database/migrations/2024_08_03_081817_create_ipay_ipay_table.php
@@ -13,7 +13,28 @@ return new class extends Migration
     {
         Schema::create('ipay_ipay', function (Blueprint $table) {
             $table->id();
-            
+
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('published')->nullable();
+            $table->string('item_id')->nullable();
+            $table->string('status')->nullable();
+            $table->string('txncd')->nullable();
+            $table->string('ivm')->nullable();
+            $table->string('qwh')->nullable();
+            $table->string('afd')->nullable();
+            $table->string('poi')->nullable();
+            $table->string('uyt')->nullable();
+            $table->string('ifd')->nullable();
+            $table->string('agd')->nullable();
+            $table->string('mc')->nullable();
+            $table->string('p1')->nullable();
+            $table->string('p2')->nullable();
+            $table->string('p3')->nullable();
+            $table->string('p4')->nullable();
+            $table->foreignId('payment_id')->nullable();
+            $table->boolean('is_processed')->nullable();
+
             $table->timestamps();
         });
     }
